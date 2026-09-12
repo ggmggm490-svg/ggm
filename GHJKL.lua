7affd33a90fa0560c247e2db231e5fd4410fc438
ggmggm490-svg <ggmggm490@gmail.com>
2026-09-12 15:32:54 +0700

    Create GHJKL.lua

---
+++ GHJKL.lua
@@ -0,0 +1,37 @@
+-- รันสคริปต์ทั้งหมดพร้อมกันโดยใช้ task.spawn เพื่อไม่ให้บล็อกการทำงานกันเอง
+
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/lennonxscripts/lennonhubv2/refs/heads/main/stealaneggv2"))()
+    end)
+end)
+
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ggmggm490-svg/ggm/refs/heads/main/RTL.lua"))()
+    end)
+end)
+
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ggmggm490-svg/ggm/refs/heads/main/RTL.lua"))()
+    end)
+end)
+
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ggmggm490-svg/ggm/refs/heads/main/RTL.lua"))()
+    end)
+end)
+
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ggmggm490-svg/ggm/refs/heads/main/RTL.lua"))()
+    end)
+end)
+
+task.spawn(function()
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ggmggm490-svg/ggm/refs/heads/main/RTL.lua"))()
+    end)
+end)
