5861f7e57b15e8d19371326e7c56fe9e0ab3a063
ggmggm490-svg <ggmggm490@gmail.com>
2026-09-12 15:39:44 +0700

    Create  GMG.lua

---
+++ GMG.lua
@@ -0,0 +1,9 @@
+-- รันสคริปต์ตัวแรก (RTL.lua) แยกออกมาต่างหาก
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ggmggm490-svg/ggm/refs/heads/main/RTL.lua"))()
+end)
+
+-- รันสคริปต์ตัวที่สอง (stealaneggv2) แยกออกมาต่างหาก
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/lennonxscripts/lennonhubv2/refs/heads/main/stealaneggv2"))()
+end)
